8f8f9f3d05ed1e5d3571e8a77e273e80ea97c352
Jerry Huang <jh24019@gmail.com>
2023-03-02 10:52:25 -0500

    .
    .

--- JavaMarket/CustomerCreator.java
+++ JavaMarket/CustomerCreator.java
@@ -1,36 +1,42 @@
 package JavaMarket;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class CustomerCreator extends Customer {
 
-	private Customer customer;
+	private Queue<Customer> customerQueue;
 
 	public CustomerCreator() {
 		super();
-		
+		customerQueue = new LinkedList<>();
 	}
 
 	public CustomerCreator(int arrivalTime, double serviceTime, double endTime, double waitTime) {
 		super(arrivalTime, serviceTime, endTime, waitTime);
-		
+
+		customerQueue = new LinkedList<>();
 	}
-	
+
 	public void Simulator() {
-		
+
 	}
-	
-	public void size() {
-		
+
+	public int size() {
+		return customerQueue.size();
 	}
-	
-	public void enqueue() {
-		
-	}
-	
-	public void frontPeek() {
-		
-	}
-	
-	public void rearPeek() {
-		
-	}
+
+	public void enqueue(Customer customer) {
+        customerQueue.add(customer);
+    }
+
+
+	public Customer frontPeek() {
+        return customerQueue.peek();
+    }
+
+	public Customer rearPeek() {
+        Customer[] customers = customerQueue.toArray(new Customer[0]);
+        return customers[customers.length - 1];
+    }
 }
